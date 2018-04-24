6889128aef801938200acad79da447e388fea03c
Juan Rivera <juanriv45@gmail.com>
2018-04-24 02:22:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,24 +6,25 @@ class Application
     if req.path.match(/item/"#{itemName}"/) then
       resp.write "#{itemName.price}"
     else
-      resp.status 
-    
+      resp.write "route not found"
+      resp.status = 404
+
   end
 
 end
 
 class Application
- 
+
   @@songs = [Song.new("Sorry", "Justin Bieber")]
- 
+
     if req.path.match(/songs/)
- 
+
       song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
       song = @@songs.find{|s| s.title == song_title}
- 
+
       resp.write song.artist
     end
- 
+
     resp.finish
   end
 end
