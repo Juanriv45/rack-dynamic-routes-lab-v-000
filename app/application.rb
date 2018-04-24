5fba5757dede34201ca489e8f8612b47d547c300
Juan Rivera <juanriv45@gmail.com>
2018-04-24 02:22:03 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/item/"#{itemName}"/) then
+      resp.write "#{itemName.price}"
+    else
+      resp.status 
+    
+  end
+
+end
+
+class Application
+ 
+  @@songs = [Song.new("Sorry", "Justin Bieber")]
+ 
+    if req.path.match(/songs/)
+ 
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
+    end
+ 
+    resp.finish
+  end
+end
